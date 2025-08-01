8667ce2297e82468b183cd70e8315548395c6872
B1ZON-c0de <roman.andreicko@icloud.com>
2025-08-01 13:18:53 +0700

    add resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,21 +16,21 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Ослеживайте Ваши Заявки И Рейтинг Резюме</h1>
           <h2>
             Просмотрите свои заявки и оцените их с помощью искуственного
             интеллекта
           </h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
-      {resumes.length > 0 && (
-        <div className="resumes-section">
-          {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </div>
-      )}
     </main>
   );
 }
